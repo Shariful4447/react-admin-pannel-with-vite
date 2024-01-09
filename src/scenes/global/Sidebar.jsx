147fc5c236ef876dac9084c251d94533912985c4
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-09 10:12:24 +0100

    Update the feedback form

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -184,7 +184,7 @@ const Sidebar = () => {
               Support
             </Typography>
             <Item
-              title="Profile Form"
+              title="Feedback Form"
               to="/form"
               icon={<PersonOutlinedIcon />}
               selected={selected}
